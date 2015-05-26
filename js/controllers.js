ac3958ac47cfa9e47a8d7e8008c04dda814c8750
padrega <paulo.adrega@gmail.com>
2015-05-26 12:11:32 +0000

    With in-memory cache

--- js/controllers.js
+++ js/controllers.js
@@ -1,33 +1,38 @@
-app.controller('PaginationController', function($scope, $timeout, parsePersistence, parseQuery) {
+app.controller('PaginationController', function($scope, $timeout, parsePersistence, parseQuery, MicroCache) {
 
     $scope.current = 1;
     $scope.items = [];
     $scope.total = 0;
     $scope.pageSize = 5; 
 
+    $scope.refresh = function() {
+        MicroCache.clear();
+        $scope.total = 0;
+        $scope.pageChanged(1);
+    }
     $scope.pageChanged = function(newPage) {
-        getResultsPage(newPage);
-    };
-
-    function getResultsPage(pageNumber) {
-        var query = parseQuery.new('TestObject').skip((pageNumber-1)*$scope.pageSize).limit($scope.pageSize);
-        
-        parseQuery.find(query)
-        .then(function(results) {
-            $scope.items = results;
-            if ($scope.total === 0) {
+        $scope.current = newPage;
+        var key = "TestObject-" + $scope.current;
+        if (MicroCache.contains(key)) {
+            $scope.items = MicroCache.get(key);
+        } else {
+            var query = parseQuery.new('TestObject').skip(($scope.current-1)*$scope.pageSize).limit($scope.pageSize);
+            
+            parseQuery.find(query)
+            .then(function(results) {
+                $scope.items = results;
+                MicroCache.set(key, results);
                 parseQuery.count(query)
                 .then(function(total) {
                     $scope.total = total;
                 }, function(error) {
                     alert(JSON.stringify(error));
                 });
-            }
-            //$scope.$apply();
-        }, function(error) {
-            alert(JSON.stringify(error));
-        });
+            }, function(error) {
+                alert(JSON.stringify(error));
+            });
+        }
     };
-    
-    getResultsPage(1);  
+
+    $scope.pageChanged(1);  
 });
---
+++ js/services.js
@@ -0,0 +1,24 @@
+/**
+ * Micro Cache
+ * - a micro library to handle a inmemory cache
+ * - works in node and browser.
+ * 
+ * @tags inmemory, keyvalue, cache, node, browser
+*/
+app.factory('MicroCache', function(){
+	var _values	= {};
+	return {
+		get	: function(key){ return _values[key];	},
+		contains: function(key){ return key in _values;	},
+		remove	: function(key){ delete _values[key];	},
+		set	: function(key, value){	_values[key] = value;},
+		values	: function(){ return _values;	},
+		clear	: function(){ _values = {};	},
+		getSet	: function(key, value){
+			if( !this.contains(key) ){
+				this.set(key, typeof value == 'function' ? value() : value )
+			}
+			return this.get(key);
+		}
+	}
+});
